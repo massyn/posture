9fbdbb45c6a70c9f8b499d7bd1e00cc1f7b5287f
Phil Massyn <phil.massyn@icloud.com>
2026-07-17 08:08:50 +1000

    Initial pypi load

---
+++ src/posture/__init__.py
@@ -0,0 +1,3 @@
+"""Continuous Controls Monitoring - Collector Library."""
+
+__version__ = "0.0.1"
---
+++ tests/test_posture.py
@@ -0,0 +1,5 @@
+import posture
+
+
+def test_version() -> None:
+    assert posture.__version__ == "0.0.1"
