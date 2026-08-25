253c574ca954df9f517bc5d6f8e19e5450ee6e74
Phil Massyn <phil.massyn@icloud.com>
2026-08-25 18:07:58 +1000

    Slack alerts

--- src/posture/__init__.py
+++ src/posture/__init__.py
@@ -35,7 +35,7 @@ logger = logging.getLogger("posture")
 load_dotenv(find_dotenv(usecwd=True))
 logger.debug("loaded .env via python-dotenv")
 
-__version__ = "0.17.4"
+__version__ = "0.17.5"
 
 __all__ = [
     "CCM",
