398e703c53dd1b9be2085d77c5178aeb3f3260ca
Phil Massyn <phil.massyn@icloud.com>
2026-09-07 10:43:06 +1000

    Bug fix on blank csv dates

--- tests/test_posture.py
+++ tests/test_posture.py
@@ -5,7 +5,7 @@ from posture.exceptions import PostureError, SourceUnknown
 
 
 def test_version() -> None:
-    assert posture.__version__ == "1.0.0"
+    assert posture.__version__ == "1.0.1"
 
 
 def test_ccm_unknown_source_raises_source_unknown() -> None:
